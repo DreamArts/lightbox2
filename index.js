10cbda1f66134e9fb1c76c8692aac1f18c3426b8
exabugs <exabugs@gmail.com>
2015-06-14 13:39:05 +0900

    webpack.

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * exabugs 2015/06/10
+ * DreamArts Corp.
+ */
+
+"use strict";
+
+require('!style!css!./css/lightbox.css');
+
+module.exports = require("imports?this=>window!./js/lightbox.js");
